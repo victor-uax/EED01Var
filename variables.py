04fd35715f4632bf6c7083dc29c3de6da2014335
Victor Segura <vseguranes@gmail.com>
2026-02-13 14:21:07 +0100

    tipos variables

---
+++ variables.py
@@ -0,0 +1,13 @@
+vi = 2
+print("vi¨:" , vi, "la , añade automáticamente un espacio")
+print("type(vi):", type(vi))
+vf = 2.0
+print("vf:",vf)
+print("Type(vf):", type(vf))
+
+vb = True #Ejemplo de comentario, los booleanos en python llevan la primera letra en mayúscula
+print("vb:," vb)
+print("type(vb):", type(vb))
+
+vs = "Hola"
+print("vs:," vs )
